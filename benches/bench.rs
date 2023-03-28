c5db84fc6e15df51c9ba8197a6d8aa224f25fc2b
Mizar <mizar.jp@gmail.com>
2023-03-29 07:25:29 +0900

    digit4

--- benches/bench.rs
+++ benches/bench.rs
@@ -20,6 +20,7 @@ fn itertools_join(bench: &mut Bencher) {
     })
 }
 
+#[allow(unused)]
 fn itertools_format(bench: &mut Bencher) {
     let values = &*VALUES;
     bench.iter(|| {
@@ -27,6 +28,7 @@ fn itertools_format(bench: &mut Bencher) {
     })
 }
 
+#[allow(unused)]
 fn foreach_print(bench: &mut Bencher) {
     let values = &*VALUES;
     bench.iter(|| {
@@ -64,13 +66,156 @@ fn foreach_print_with_bufwriter(bench: &mut Bencher) {
     })
 }
 
+const DIGIT4: [u8; 40000] = gen_digit4();
+const fn gen_digit4() -> [u8; 40000] {
+    let (mut d4, mut i) = ([0u8; 40000], 0);
+    // const fn while: Rust 1.46.0 or later
+    while i < 1_0000 {
+        d4[i * 4] = b'0' + ((i / 100) / 10) as u8;
+        d4[i * 4 + 1] = b'0' + ((i / 100) % 10) as u8;
+        d4[i * 4 + 2] = b'0' + ((i % 100) / 10) as u8;
+        d4[i * 4 + 3] = b'0' + ((i % 100) % 10) as u8;
+        i += 1;
+    }
+    d4
+}
+unsafe fn dappend_bytes(v: &mut Vec<u8>, x: usize) {
+    match x {
+        0..=9 => v.extend_from_slice(&(&DIGIT4[(x * 4 + 3)..])[..1]),
+        10..=99 => v.extend_from_slice(&(&DIGIT4[(x * 4 + 2)..])[..2]),
+        100..=999 => v.extend_from_slice(&(&DIGIT4[(x * 4 + 1)..])[..3]),
+        1000..=9999 => v.extend_from_slice(&(&DIGIT4[(x * 4)..])[..4]),
+        1_0000..=9_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.extend_from_slice(&(&DIGIT4[(y0 * 4 + 3)..])[..1]);
+            v.extend_from_slice(&(&DIGIT4[(y1 * 4)..])[..4]);
+        }
+        10_0000..=99_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.extend_from_slice(&(&DIGIT4[(y0 * 4 + 2)..])[..2]);
+            v.extend_from_slice(&(&DIGIT4[(y1 * 4)..])[..4]);
+        }
+        100_0000..=999_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.extend_from_slice(&(&DIGIT4[(y0 * 4 + 1)..])[..3]);
+            v.extend_from_slice(&(&DIGIT4[(y1 * 4)..])[..4]);
+        }
+        1000_0000..=9999_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.extend_from_slice(&(&DIGIT4[(y0 * 4)..])[..4]);
+            v.extend_from_slice(&(&DIGIT4[(y1 * 4)..])[..4]);
+        }
+        _ => {
+            let (z0, z1) = (x / 1_0000_0000, x % 1_0000_0000);
+            let (y0, y1) = (z1 / 1_0000, z1 % 1_0000);
+            dappend_bytes(v, z0);
+            v.extend_from_slice(&(&DIGIT4[(y0 * 4)..])[..4]);
+            v.extend_from_slice(&(&DIGIT4[(y1 * 4)..])[..4]);
+        }
+    }
+}
+unsafe fn dappend_str(v: &mut String, x: usize) {
+    use std::str::from_utf8_unchecked;
+    match x {
+        0..=9 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4 + 3)..])[..1])),
+        10..=99 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4 + 2)..])[..2])),
+        100..=999 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4 + 1)..])[..3])),
+        1000..=9999 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4)..])[..4])),
+        1_0000..=9_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4 + 3)..])[..1]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        10_0000..=99_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4 + 2)..])[..2]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        100_0000..=999_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4 + 1)..])[..3]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        1000_0000..=9999_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4)..])[..4]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        _ => {
+            let (z0, z1) = (x / 1_0000_0000, x % 1_0000_0000);
+            let (y0, y1) = (z1 / 1_0000, z1 % 1_0000);
+            dappend_str(v, z0);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4)..])[..4]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+    }
+}
+
+fn digit4_bytes(bench: &mut Bencher) {
+    let values = &*VALUES;
+    bench.iter(|| {
+        let mut stderr = io::stderr();
+        let mut buf = values.iter().fold(Vec::<u8>::new(), |mut acc, &cur| {
+            unsafe { dappend_bytes(&mut acc, cur) };
+            acc.push(b' ');
+            acc
+        });
+        buf.pop();
+        buf.push(b'\n');
+        stderr.write_all(&buf).unwrap();
+    })
+}
+
+fn digit4_str(bench: &mut Bencher) {
+    let values = &*VALUES;
+    bench.iter(|| {
+        let mut stderr = io::stderr();
+        writeln!(
+            stderr,
+            "{}",
+            values
+                .iter()
+                .fold(String::new(), |mut acc, &cur| {
+                    unsafe { dappend_str(&mut acc, cur) };
+                    acc.push(' ');
+                    acc
+                })
+                .trim_end()
+        )
+        .unwrap();
+    })
+}
+
+fn digit4_str_with_bufwriter(bench: &mut Bencher) {
+    let values = &*VALUES;
+    bench.iter(|| {
+        let mut stderr = io::BufWriter::new(io::stderr().lock());
+        writeln!(
+            stderr,
+            "{}",
+            values
+                .iter()
+                .fold(String::with_capacity(7 * values.len()), |mut acc, &cur| {
+                    unsafe { dappend_str(&mut acc, cur) };
+                    acc.push(' ');
+                    acc
+                })
+                .trim_end()
+        )
+        .unwrap();
+    })
+}
+
 benchmark_group!(
     benches,
     itertools_join,
-    itertools_format,
-    foreach_print,
+    //itertools_format,
+    //foreach_print,
     itertools_join_with_bufwriter,
     itertools_format_with_bufwriter,
     foreach_print_with_bufwriter,
+    digit4_bytes,
+    digit4_str,
+    digit4_str_with_bufwriter,
 );
 benchmark_main!(benches);
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn to_string_and_join(values: &[usize]) -> String {
+pub fn to_string_and_join(values: &[usize]) -> String {
     values
         .iter()
         .map(|x| x.to_string())
@@ -13,41 +13,41 @@ fn to_string_and_join(values: &[usize]) -> String {
         .join(" ")
 }
 
-fn fold_by_to_string_without_capacity(values: &[usize]) -> String {
+pub fn fold_by_to_string_without_capacity(values: &[usize]) -> String {
     values
         .iter()
         .map(|s| s.to_string())
         .fold(String::new(), |mut acc, cur| {
             acc.push_str(&cur);
-            acc.push_str(" ");
+            acc.push(' ');
             acc
         })
 }
 
-fn fold_by_to_string_with_capacity(values: &[usize]) -> String {
+pub fn fold_by_to_string_with_capacity(values: &[usize]) -> String {
     values
         .iter()
         .map(|s| s.to_string())
         // 数値一つが最大6桁 + separator1文字で7文字
         .fold(String::with_capacity(7 * values.len()), |mut acc, cur| {
             acc.push_str(&cur);
-            acc.push_str(" ");
+            acc.push(' ');
             acc
         })
 }
 
-fn itertools_join(values: &[usize]) -> String {
+pub fn itertools_join(values: &[usize]) -> String {
     values.iter().join(" ")
 }
 
-fn fold_by_write_without_capacity(values: &[usize]) -> String {
+pub fn fold_by_write_without_capacity(values: &[usize]) -> String {
     values.iter().fold(String::new(), |mut acc, cur| {
         write!(&mut acc, "{} ", cur).unwrap();
         acc
     })
 }
 
-fn fold_by_write_with_capacity(values: &[usize]) -> String {
+pub fn fold_by_write_with_capacity(values: &[usize]) -> String {
     values
         .iter()
         // 数値一つが最大6桁 + separator1文字で7文字
@@ -57,13 +57,75 @@ fn fold_by_write_with_capacity(values: &[usize]) -> String {
         })
 }
 
+const DIGIT4: [u8; 40000] = gen_digit4();
+const fn gen_digit4() -> [u8; 40000] {
+    let (mut d4, mut i) = ([0u8; 40000], 0);
+    // const fn while: Rust 1.46.0 or later
+    while i < 1_0000 {
+        d4[i * 4] = b'0' + ((i / 100) / 10) as u8;
+        d4[i * 4 + 1] = b'0' + ((i / 100) % 10) as u8;
+        d4[i * 4 + 2] = b'0' + ((i % 100) / 10) as u8;
+        d4[i * 4 + 3] = b'0' + ((i % 100) % 10) as u8;
+        i += 1;
+    }
+    d4
+}
+unsafe fn dappend(v: &mut String, x: usize) {
+    use std::str::from_utf8_unchecked;
+    match x {
+        0..=9 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4 + 3)..])[..1])),
+        10..=99 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4 + 2)..])[..2])),
+        100..=999 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4 + 1)..])[..3])),
+        1000..=9999 => v.push_str(from_utf8_unchecked(&(&DIGIT4[(x * 4)..])[..4])),
+        1_0000..=9_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4 + 3)..])[..1]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        10_0000..=99_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4 + 2)..])[..2]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        100_0000..=999_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4 + 1)..])[..3]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        1000_0000..=9999_9999 => {
+            let (y0, y1) = (x / 1_0000, x % 1_0000);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4)..])[..4]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+        _ => {
+            let (z0, z1) = (x / 1_0000_0000, x % 1_0000_0000);
+            let (y0, y1) = (z1 / 1_0000, z1 % 1_0000);
+            dappend(v, z0);
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y0 * 4)..])[..4]));
+            v.push_str(from_utf8_unchecked(&(&DIGIT4[(y1 * 4)..])[..4]));
+        }
+    }
+}
+
+pub fn digit4_with_capacity(values: &[usize]) -> String {
+    let mut acc = values
+        .iter()
+        .fold(String::with_capacity(7 * values.len()), |mut acc, &cur| {
+            unsafe { dappend(&mut acc, cur) };
+            acc.push(' ');
+            acc
+        });
+    acc.pop();
+    acc
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test() {
-        let arr = (1..10).collect::<Vec<_>>();
+        let arr = (0..1000000).collect::<Vec<_>>();
 
         let results = [
             to_string_and_join(&arr),
@@ -72,6 +134,7 @@ mod tests {
             itertools_join(&arr),
             fold_by_write_without_capacity(&arr),
             fold_by_write_with_capacity(&arr),
+            digit4_with_capacity(&arr),
         ];
 
         assert!(
